d0ca24641403933ae8a5099f3a0b75740d916206
Enrique Yishaq German Totosaus <yishaq95@gmail.com>
2018-10-06 14:30:38 -0500

    Optain average sampling

--- sensor.py
+++ sensor.py
@@ -6,8 +6,17 @@ a_gas_port = 0
 d_gas_port = 2
 
 def main():
-    input_port = mraa.Aio( a_gas_port )
-    print( input_port.read() )
+    while True:
+        input_port = mraa.Aio( a_gas_port )
+        print( sample(input_port) )
+
+def sample( input_port ):
+    max_range_value = 100
+    result = 0
+    for i in range(0, max_range_value):
+        result += input_port.read()
+
+    return result / max_range_value
 
 if __name__ == '__main__':
     main()
