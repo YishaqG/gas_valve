2a02a72bb7e9cf9630b88bd7309d092a961d7515
Enrique Yishaq German Totosaus <yishaq95@gmail.com>
2018-10-06 21:13:04 -0500

    Test2(Servo)

--- sensor.py
+++ sensor.py
@@ -1,14 +1,21 @@
 #!/usr/local/bin/python
 
 import mraa
+from lib.Servo import Servo
 
 a_gas_port = 0
 d_gas_port = 2
 
 def main():
-    while True:
-        input_port = mraa.Aio( a_gas_port )
-        print( sample(input_port) )
+    # while True:
+    #     input_port = mraa.Aio( a_gas_port )
+    #     print( sample(input_port) )
+
+    i = 0
+    servo = Servo(3)
+    while i < 180:
+        servo.write(i)
+        i += 5
 
 def sample( input_port ):
     max_range_value = 100
